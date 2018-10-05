5bcdf9404e63b1385f8bb862f4b0ea1635906afd
hakaio <pranshu2009@gmail.com>
2018-10-05 16:08:07 +0530

    added address file reading

--- vmm.c
+++ vmm.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
+#include <stdint.h>
+#include<stdlib.h>
+
 #define PAGEMASK 0xFF00
 #define OFFSETMASK 0x00FF
-unsigned int extractPageNumber(int num){
+
+unsigned short int extractPageNumber(uint16_t num){
   return (num & PAGEMASK) >> 8;
 }
-unsigned int extractOffset(int num){
+unsigned short int extractOffset(uint16_t num){
   return num & OFFSETMASK;
 }
 int main(){
-  unsigned int address;
-  printf("Enter a address\n");
-  scanf("%x",&address);
-  printf("\nPage number : %x and Offset : %x",extractPageNumber(address),extractOffset(address));
+  uint16_t address;
+  FILE *logicalAddressStream;
+
+  logicalAddressStream = fopen("address.txt","r");
+  if(logicalAddressStream == NULL){
+  	printf("Error opening the address file");
+  	exit(1);
+  }
+  else{
+  	while(fscanf(logicalAddressStream,"%x",&address) != EOF){
+  		  printf("\nPage number : %x and Offset : %x",extractPageNumber(address),extractOffset(address));
+  	}
+  }
+  fclose(logicalAddressStream);
   return 0;
 }
