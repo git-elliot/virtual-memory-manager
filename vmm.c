429f3467dce45a8d38a2306ff9def944af5614ba
hakaio <pranshu2009@gmail.com>
2018-10-05 14:33:40 +0530

    Minor changes

--- vmm.c
+++ vmm.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #define PAGEMASK 0xFF00
 #define OFFSETMASK 0x00FF
-int extractPageNumber(int num){
-  return num & PAGEMASK;
+unsigned int extractPageNumber(int num){
+  return (num & PAGEMASK) >> 8;
 }
-int extractOffset(int num){
+unsigned int extractOffset(int num){
   return num & OFFSETMASK;
 }
 int main(){
-  int address;
+  unsigned int address;
   printf("Enter a address\n");
   scanf("%x",&address);
   printf("\nPage number : %x and Offset : %x",extractPageNumber(address),extractOffset(address));
